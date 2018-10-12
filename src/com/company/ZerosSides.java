d57bdf09d7f4148ef83585acf3530e3c5db23fbb
gimeno <r.gimenocallejas@gmail.com>
2018-10-12 12:22:52 +0100

    New methods to move the zeros

--- src/com/company/ZerosSides.java
+++ src/com/company/ZerosSides.java
@@ -18,32 +18,16 @@ public class ZerosSides {
 //        System.out.println(Arrays.toString(zerosToLeft(arr)));
 //        System.out.println(Arrays.toString(zerosToRight3(arr)));
 
-        System.out.println(Arrays.toString(zerosToLeft(new int[]{0,0,3,0,8,9,0,4,8,2})));
-//        System.out.println(Arrays.toString(zerosToLeft2(new int[]{0,0,3,0,8,9,0,4,8,2})));
-//        System.out.println(Arrays.toString(zerosToRight(new int[]{0,0,3,0,8,9,0,4,8,2})));
-        System.out.println(Arrays.toString(zerosToRight2(new int[]{0,0,3,0,8,9,0,4,8,2})));
-//        System.out.println(Arrays.toString(zerosToRight3(new int[]{0,0,3,0,8,9,0,4,8,2})));
+        System.out.println(Arrays.toString(new int[]{0, 0, 3, 0, 8, 9, 0, 4, 8, 2}));
+        System.out.println("To Left lineal\t\t\t" + Arrays.toString(zerosToLeftLineal(new int[]{0, 0, 3, 0, 8, 9, 0, 4, 8, 2})));
+        System.out.println("To Left sorting\t\t\t" + Arrays.toString(zerosToLeftSorting(new int[]{0, 0, 3, 0, 8, 9, 0, 4, 8, 2})));
+        System.out.println("To Left brute force\t\t" + Arrays.toString(zerosToLeftBruteForce(new int[]{0, 0, 3, 0, 8, 9, 0, 4, 8, 2})));
+        System.out.println("To right lineal\t\t\t" + Arrays.toString(zerosToRightLineal(new int[]{0, 0, 3, 0, 8, 9, 0, 4, 8, 2})));
+        System.out.println("To right sorting\t\t" + Arrays.toString(zerosToRightSorting(new int[]{0, 0, 3, 0, 8, 9, 0, 4, 8, 2})));
+        System.out.println("To right brute force\t" + Arrays.toString(zerosToRightBruteForce(new int[]{0, 0, 3, 0, 8, 9, 0, 4, 8, 2})));
     }
 
-    private static int[] zerosToRight(int[] arr) {
-        int left = 0;
-        int right = arr.length - 1;
-        while (right >= left) {
-            if (arr[left] != 0) {
-                left++;
-            }
-
-            if (arr[right] != 0 && arr[left] == 0) {
-                arr[left] = arr[right];
-                right--;
-                left++;
-            }
-        }
-        System.out.println(left);
-        return arr;
-    }
-
-    private static int[] zerosToRight2(int[] arr) {
+    private static int[] zerosToRightLineal(int[] arr) {
         int p = 0;
         int d = 0;
         while (p < arr.length) {
@@ -55,7 +39,6 @@ public class ZerosSides {
                 p++;
             }
         }
-        System.out.println(d);
         while (d < arr.length) {
             arr[d] = 0;
             d++;
@@ -63,7 +46,7 @@ public class ZerosSides {
         return arr;
     }
 
-    private static int[] zerosToRight3(int[] array) {
+    private static int[] zerosToRightSorting(int[] array) {
         Arrays.sort(array);
         for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
@@ -73,12 +56,29 @@ public class ZerosSides {
         return array;
     }
 
-    private static int[] zerosToLeft2(int[] arr) {
+    private static int[] zerosToRightBruteForce(int[] arr) {
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                for (int j = arr.length - 1; j > i; j--) {
+                    if (arr[j] != 0) {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    private static int[] zerosToLeftSorting(int[] arr) {
         Arrays.sort(arr);
         return arr;
     }
 
-    private static int[] zerosToLeft(int[] arr) {
+    private static int[] zerosToLeftLineal(int[] arr) {
         int p = arr.length - 1;
         int d = p;
         while (p >= 0) {
@@ -90,11 +90,27 @@ public class ZerosSides {
                 p--;
             }
         }
-        System.out.println(arr.length - d);
         while (d >= 0) {
             arr[d] = 0;
             d--;
         }
+        return arr;
+    }
+
+    private static int[] zerosToLeftBruteForce(int[] arr) {
+        int temp;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == 0) {
+                for (int j = 0; j < i; j++) {
+                    if (arr[j] != 0) {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+
         return arr;
     }
 
